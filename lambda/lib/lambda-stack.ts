1083523035076a21952131644e8ba5f5c9e8cf13
Bruno Higino de Souza <brunohigino06@gmail.com>
2023-04-06 19:11:12 -0300

    06.04.2023

--- lambda/lib/lambda-stack.ts
+++ lambda/lib/lambda-stack.ts
@@ -1,6 +1,7 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 
 export class LambdaStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -13,5 +14,10 @@ export class LambdaStack extends Stack {
       memorySize: 1024,
       runtime: lambda.Runtime.NODEJS_14_X,
     });
+
+    new apigw.LambdaRestApi(this, 'APIPipefy', {
+      handler: PipefyFunction
+    });
+
   }
 }
